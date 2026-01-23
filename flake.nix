6ce5bcd1b9b985ca3a4497160a49cd0af26d32fd
avolord <the.avo.lord@gmail.com>
2026-01-23 10:41:54 +0100

    added quickshell to flake

--- flake.nix
+++ flake.nix
@@ -1,10 +1,33 @@
 {
-  outputs = { self, nixpkgs }: {
-    nixosConfigurations.myhost = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./configuration.nix  # Your existing config
-      ];
+  description = "My NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    
+    quickshell = {
+      url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, quickshell }: {
+    nixosConfigurations = {
+      # Replace "myhost" with your actual hostname
+      # Find it with: hostname
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";  # or "aarch64-linux" for ARM
+        
+        modules = [
+          ./configuration.nix
+          
+          # Make quickshell available
+          ({ pkgs, ... }: {
+            environment.systemPackages = [
+              quickshell.packages.${pkgs.system}.default
+            ];
+          })
+        ];
+      };
     };
   };
 }
